2b91a05fcaef42f16337752a724cc31f04563b77
Ben Bornholm <thunderwagon@Bens-MacBook-Pro.local>
2023-02-11 18:32:55 -0600

    Initial PoC for BTV teleport TF module

---
+++ terraform/ec2.tf
@@ -0,0 +1,102 @@
+############################################ Security group ############################################
+resource "aws_security_group" "teleport_cluster" {
+  name        = "${var.PROJECT_PREFIX}-teleport-cluster-sg"
+  description = "${var.PROJECT_PREFIX} Teloeport Cluster Security Group"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Project = var.PROJECT_PREFIX
+  }
+}
+
+resource "aws_security_group_rule" "web_interface" {
+  description       = "In TLS Routing mode, the Proxy handles all protocols, including Web UI, HTTPS, Kubernetes, SSH, and all databases on a single port."
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.teleport_cluster.id
+}
+
+resource "aws_security_group_rule" "proxy" {
+  description       = "Port used by Teleport Proxy Service instances to dial agents in Proxy Peering mode."
+  type              = "ingress"
+  from_port         = 3021
+  to_port           = 3021
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.teleport_cluster.id
+}
+
+resource "aws_security_group_rule" "ssh_proxy" {
+  description       = "SSH port. This is Teleports equivalent of port 22 for SSH. Only used when Teleport Node is replacing SSH."
+  type              = "ingress"
+  from_port         = 3022
+  to_port           = 3022
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.teleport_cluster.id
+}
+
+resource "aws_security_group_rule" "auth_proxy" {
+  description       = "TLS port used by the Auth Service to serve its API to other Nodes in a cluster."
+  type              = "ingress"
+  from_port         = 3025
+  to_port           = 3025
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.teleport_cluster.id
+}
+
+resource "aws_security_group_rule" "rdp_proxy" {
+  description       = "When using Desktop Service windows_desktop_service.listen_addr"
+  type              = "ingress"
+  from_port         = 3028
+  to_port           = 3028
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.teleport_cluster.id
+}
+
+resource "aws_security_group_rule" "outbound" {
+  description       = "Allow egress traffic"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.teleport_cluster.id
+}
+
+############################################ EC2 ############################################
+resource "aws_instance" "teleport" {
+  key_name               = var.public_key_name
+  ami                    = var.ubunut-ami
+  instance_type          = var.instance_type
+  subnet_id              = var.teleport_subnet_id
+  vpc_security_group_ids = [aws_security_group.teleport_cluster.id]
+  iam_instance_profile   = aws_iam_role.teleport.id
+
+  metadata_options {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
+
+  tags = {
+    Project = var.PROJECT_PREFIX
+  }
+}
+
+resource "aws_eip" "telelport" {
+  vpc      = true
+  instance = aws_instance.teleport.id
+
+  tags = {
+    Project = var.PROJECT_PREFIX
+  }
+}
---
+++ terraform/s3.tf
@@ -0,0 +1,57 @@
+/*
+Configuration of S3 bucket for certs and replay
+storage. Uses server side encryption to secure
+session replays and SSL certificates.
+*/
+
+// S3 bucket for cluster storage
+// For demo purposes, don't need bucket logging
+// tfsec:ignore:aws-s3-enable-bucket-logging
+
+resource "random_string" "bucket_suffix" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "aws_s3_bucket_public_access_block" "teleport" {
+  bucket = aws_s3_bucket.teleport.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "teleport" {
+  bucket = aws_s3_bucket.teleport.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "teleport" {
+  # Enable server-side encryption by default
+  bucket = aws_s3_bucket.teleport.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_acl" "teleport" {
+  bucket = aws_s3_bucket.teleport.bucket
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "teleport" {
+  bucket        = "${var.PROJECT_PREFIX}-teleport-${random_string.bucket_suffix.result}"
+  force_destroy = true
+
+  tags = {
+    Project = var.PROJECT_PREFIX
+  }
+}
