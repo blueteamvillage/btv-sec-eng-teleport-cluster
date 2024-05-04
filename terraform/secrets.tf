3bcb78cdf7316b2997393e7b3f7526a75f530ebb
Chris Maenner <3287968+cmaenner@users.noreply.github.com>
2024-05-04 18:26:25 -0400

    Fix misspelling

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "github_oauth_secret" {
-  name = "${var.PROJECT_PREFIX}-teleport-github-ouath-secret"
+  name = "${var.PROJECT_PREFIX}-teleport-github-oauth-secret"
 
   tags = {
     Project = var.PROJECT_PREFIX
