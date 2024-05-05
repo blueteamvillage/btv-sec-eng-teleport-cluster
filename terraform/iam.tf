f939bea59dd0ad9e68c25d9efac732537ef57aff
Chris Maenner <3287968+cmaenner@users.noreply.github.com>
2024-05-04 22:12:38 -0400

    Add new variable to create policy document for IAM

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,7 +20,7 @@ data "aws_iam_policy_document" "teleport" {
 
 resource "aws_iam_role" "teleport" {
   name               = "${var.PROJECT_PREFIX}-teleport-cluster"
-  assume_role_policy = data.aws_iam_policy_document.teleport.json
+  assume_role_policy = "${var.teleport_app_serv_role_policy}"
 
   tags = {
     Project = var.PROJECT_PREFIX
