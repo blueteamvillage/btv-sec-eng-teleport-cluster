22c498585cfd6389430e510275196f3ecb1c7f4e
Chris Maenner <3287968+cmaenner@users.noreply.github.com>
2024-05-12 20:47:28 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "public_ip_addr" {
 output "private_ip_addr" {
   value = aws_instance.teleport.private_ip
 }
+
+output "teleport_instance_profile_role_name" {
+  value = aws_iam_role.teleport.id
+}
+
+output "teleport_sg_id" {
+  value = aws_security_group.teleport_cluster.id
+}
